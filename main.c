89cb53354ecd788d6cda1dec5b943f2e8253ea9d
Glucthu <glucthu@protonmail.com>
2021-04-12 03:27:03 -0300

    testing dynamic arrays

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int *array;
+  size_t used;
+  size_t size;
+} vector;
+
+void initVector(vector *a, size_t initialSize) {
+  a->array = malloc(initialSize * sizeof(int));
+  a->used = 0;
+  a->size = initialSize;
+}
+
+void insertVector(vector *a, int element) {
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = realloc(a->array, a->size * sizeof(int));
+  }
+  a->array[a->used++] = element;
+}
+
+void freeVector(vector *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
+}
 
 int main()
 {
+	vector nums;
+	int i;
+
+	initVector(&nums, 1);  // initially 5 elements
+	//for (i = 0; i < 100; i++)
+	//  insertVector(&nums, i);  // automatically resizes as necessary
+	freeVector(&nums);
 	return 0;
 }
