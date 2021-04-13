27a5b9f69e09818aae5177b0910f6d4c263abd9b
Glucthu <glucthu@protonmail.com>
2021-04-13 06:03:43 -0300

    added game function called memory

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define clscr() printf("\e[1;1H\e[2J")
+
+void memory()
+{
+	unsigned short int level = 1; 
+	unsigned short int lifes = 3;
+	unsigned short int *nums;
+	char ans; //answer
+	
+	nums = calloc(level+1, sizeof(unsigned short int)); //nums[level+1];
+	srand(time(NULL));
+
+	nums[0] = rand()%100+1; //random number between 1 - 100
+	printf("First number: %d\n", nums[0]);
+	getchar();
+
+	while(lifes > 0)
+	{
+		clscr();
+
+		nums[level+1] = rand()%100+1;
+		printf("is that number equal to %d number(s) ago?", level);
+		ans = getchar();
+	}
+
+}
 
 int main()
 {
+	memory();
 	return 0;
 }
