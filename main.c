b081a72f1bd82f4fe7aaae9c00b1e629f188970e
Glucthu <glucthu@protonmail.com>
2021-04-15 05:23:17 -0300

    added point system

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ void memory()
 {
 	unsigned short int level = 1; 
 	unsigned short int lifes = 3;
+	unsigned short int points = 0;
 	unsigned short int *nums;
 	char ans; //answer
 	
@@ -22,17 +23,26 @@ void memory()
 	{
 		clscr();
 		printf("Lifes: %d\n", lifes);
-		
+		printf("Points: %d\n", points);
+		printf("\n");
+
 		nums[level] = rand()%5+1;
 		printf("is %d equal to %d number(s) ago?\n", nums[level], level);
 		printf("> "); scanf(" %c", &ans);
 
 		if((ans == 's' && nums[0] != nums[level]) || (ans == 'n' && nums[0] == nums[level]))
 			lifes--;
+		else
+			points++;
 
 		for(unsigned short int i = 0; i < level; i++)
 			nums[i] = nums[i+1];
-		
+
+		if(points % 3 == 0)
+		{
+			level++;
+			max_num *= ;
+		}
 	}
 	
 	free(nums);
