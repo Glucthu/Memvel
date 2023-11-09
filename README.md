abf7ab25e6199935e077df109fcc7b4fd6027233
Glucthu <glucthu@protonmail.com>
2023-11-09 20:35:08 -0300

    changed rules, description and remade half of the memory (now called game) function

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # Memory game
 
 ## Usage
-You firstly will see a random number, remember it, then the game starts.
-You have to remember if you saw a number exactly n times ago, and answer (y)es or (n)o if it was there.
+This is a simple terminal verion of the classic n-back game.
+
+The game shows random numbers (optional: letters and symbols) and the player have to remember if the current number is equal to the previous number n possitions ago.
+
+eg:
+
+n=2
+1, 3, 6 (2 numbers ago), 7, 4 <-- current number
+the game will ask if you have seen the current number(4) n(2) possitions ago
+seen (y/n): n
+
+n=2
+1 (2 numbers ago), 5, 2, 1 <-- current number
+seen (y/n): y
+
+1. Enter the value of n.
+2. You will be asked if you want to include letters and symbols, it makes the game a little harder :).
+3. Each time you will see a random number on the screen and you have to enter if you have seen it n possitions ago.
--- main.c
+++ main.c
@@ -2,57 +2,91 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define clrscr() printf("\e[1;1H\e[2J")
 
-void memory()
+void print_game_gui_header(unsigned short int lives, unsigned short int points){
+	clrscr();
+	printf("Lives: %d\n", lives);
+	printf("Points: %d\n", points);
+	printf("\n");
+}
+
+bool ask_for_char(){
+	char include_char_ans;
+
+
+	while(	include_char_ans != 'n' && include_char_ans != 'N' &&
+		include_char_ans != 's' && include_char_ans != 'S' ) {
+		printf("Include letters and symbols? (y/n): ");
+		scanf(" %c", &include_char_ans);
+	}
+
+	if(include_char_ans == 's' || include_char_ans == 'S')
+		return true;
+	return false;
+}
+
+void game()
 {
-	unsigned short int level = 1; 
-	unsigned short int lifes = 3;
+	const unsigned short int questions_per_level = 25;
+	unsigned short int n;
+	bool include_char;
+	unsigned short int lives = 3;
 	unsigned short int points = 0;
-	unsigned short int max_rnum = 5; //max random number
-	unsigned short int *nums;
+	unsigned short int current_question = 0;
+	unsigned short int max_rnum = 9; //max random number
+	char *item_list;
 	char ans; //answer
 	
-	nums = calloc(level, sizeof(unsigned short int)); //nums[level];
+	item_list = calloc(questions_per_level, sizeof(char)); //nums[questions_per_level];
 	srand(time(NULL));
 
-	nums[0] = rand()%max_rnum+1; //random number between 1 - 5
-	clrscr();
-	printf("First number: %d\n", nums[0]);
-	getchar();
+	item_list[0] = rand()%max_rnum+1; //random number between 1 - 5
+	//clrscr();
+	//printf("First number: %d\n", item_list[0]);
+	//getchar();
 
-	while(lifes > 0)
-	{
+	printf("Value of n: "); scanf("%hu", &n);
+	include_char = ask_for_char();
+
+	//while(lives > 0)
+	//{
+		//print_game_gui_header(lives, points);
+		//item_list[current_question] = rand()%max_rnum+1;
+
+
+		/*
 		clrscr();
 		printf("Lifes: %d\n", lifes);
 		printf("Points: %d\n", points);
 		printf("\n");
-
-		nums[level] = rand()%max_rnum+1;
-		printf("is %d equal to %d number(s) ago?\n", nums[level], level);
+		*/
+		//nums[level] = rand()%max_rnum+1;
+		/*printf("is %d equal to %d number(s) ago?\n", item_list[current_question], n);
 		printf("> "); scanf(" %c", &ans);
 
-		if((ans == 's' && nums[0] != nums[level]) || (ans == 'n' && nums[0] == nums[level]))
-			lifes--;
+		if((ans == 'y' && item_list[0] != item_list[points]) || (ans == 'n' && item_list[0] == item_list[points]))
+			lives--;
 		else
 			points++;
 
-		for(unsigned short int i = 0; i < level; i++)
-			nums[i] = nums[i+1];
+		for(unsigned short int i = 0; i < points; i++)
+			item_list[i] = item_list[i+1];
 
-		if(points % (int)pow(level * 1.7, 2) == 0)
+		if(points % (int)pow(n * 1.7, 2) == 0)
 		{
-			level++;
+			n++;
 			max_rnum *= 1.3;
 		}
-	}
+	}*/
 	
-	free(nums);
+	//free(item_list);
 }
 
 int main()
 {
-	memory();
+	game();
 	return 0;
 }
